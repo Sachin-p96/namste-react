d94440a44622edff14b992ed8227122e7ce83293
sachin P <sachin.puthiyaparamban@gmail.com>
2024-05-03 17:37:46 +0530

    useSelector, UseDispatch

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,11 +2,12 @@ import { LOGO_URL } from "../utils/constants";
 import { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import UserContext from "../utils/UserContext";
+import { useSelector } from "react-redux";
 export const Header = () => {
   const [btnName, setbtnName] = useState("Login");
   const { loggedUser } = useContext(UserContext);
   const { setName } = useContext(UserContext);
-  console.log(loggedUser,'hahahahahah')
+  console.log(loggedUser, "hahahahahah");
   const [userName, setUserName] = useState(loggedUser);
   const handleButtonChange = () => {
     if (btnName == "Login") {
@@ -17,6 +18,7 @@ export const Header = () => {
       setName("Sachin Out");
     }
   };
+  const cartitems = useSelector((store) =>  store.cart.items);
   return (
     <div className="flex justify-between bg-orange-400 brightness-125">
       <div className="logo-container">
@@ -36,7 +38,9 @@ export const Header = () => {
           <li className="px-4 font-serif font-bold">
             <Link to="/grocerry">Grocerry Mart</Link>
           </li>
-          <li className="px-4 font-serif font-bold">Cart</li>
+          <li className="px-4 font-serif font-bold">
+            Cart -- {cartitems.length} items
+          </li>
 
           <button onClick={handleButtonChange}>{btnName}</button>
           <li className="px-4 font-serif font-bold">{loggedUser}</li>
--- src/components/RenderItemsSection.js
+++ src/components/RenderItemsSection.js
@@ -1,13 +1,19 @@
+import { useDispatch } from "react-redux"
+import { addItem } from "../utils/cartSlice"
+
 const RenderItemsSection = ({card}) => {
    const {name,defaultPrice,price} = card?.info
+   const dispatch = useDispatch()
+   const handleAddItem = () => {
+       dispatch(addItem('Pizzza'))
+   }
    return (
-    <div className="border-b-2 flex justify-between">
+    <div className="border-b-2 flex justify-between p-2">
        <div className="text-left">
-          {name}
-       </div>
-       <div>
+          {name} ---
           {defaultPrice/100 || price/100}
        </div>
+       <button onClick = {handleAddItem} className="border border-black rounded-md p-2 mr-1 bg-black text-white ">Add +</button>
        </div>
    )
 }
